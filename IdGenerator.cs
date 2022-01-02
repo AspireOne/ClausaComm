b1e7318d9d706b723e6f57d010e4b91f9bd549e8
Matěj Pešl <matejpesl1@gmail.com>
2022-01-03 00:15:44 +0100

    add a few chracters to id generator

--- IdGenerator.cs
+++ IdGenerator.cs
@@ -8,19 +8,21 @@ namespace ClausaComm
         // 61^8 = 191,707,312,997,281 (191 bilionů (trillion) 707 miliard (billion) 312 milionů (million)...)
         private const byte DefaultLength = 8;
         private static readonly StringBuilder IdBuilder = new(DefaultLength);
+        // All chars are single-byte in UTF-8.
         private static readonly char[] AllowedChars = {
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
                 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '~', '#', '=', '_',
+                '$', '%', '&', '-', '+'
         };
 
-        public static string GenerateId(int length = DefaultLength)
+        public static string GenerateId(uint length = DefaultLength)
         {
-            for (var i = 0; i < length; ++i)
+            for (uint i = 0; i < length; ++i)
                 IdBuilder.Append(AllowedChars[Constants.Random.Next(0, AllowedChars.Length)]);
 
-            var id = IdBuilder.ToString();
+            string id = IdBuilder.ToString();
             IdBuilder.Clear();
             return id;
         }
