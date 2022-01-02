4e34de745fac0965ccb78f24c381e15d0628b992
Matěj Pešl <matejpesl1@gmail.com>
2022-01-03 00:15:14 +0100

    Rebuild message implementation

---
+++ Messages/ChatMessage.cs
@@ -0,0 +1,32 @@
+﻿using ClausaComm.Network_Communication.Objects;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ClausaComm.Messages
+{
+    public struct ChatMessage : ISendable
+    {
+        public enum Ways { In, Out }
+        public RemoteObject.ObjectType ObjectType => RemoteObject.ObjectType.ChatMessage;
+        public readonly Ways Way;
+        public readonly string Text;
+        //public readonly MessageFile File;
+        public readonly string Id;
+        public const int MaxTextLength = 6000;
+
+        public ChatMessage(string text, Ways way = Ways.Out)
+        {
+            if (text?.Length > MaxTextLength)
+                throw new ArgumentException("The text is too long.", nameof(text));
+            
+            Text = text;
+            Id = IdGenerator.GenerateId(5);
+            Way = way;
+        }
+
+        private ChatMessage(string text, Ways way, string id) : this(text, way) => Id = id;
+
+        public static ChatMessage ReconstructMessage(string text, Ways way, string id) =>
+            new ChatMessage(text, way, id);
+    }
+}
---
+++ Messages/MessagesXml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Xml.Linq;
+
+namespace ClausaComm.Messages
+{
+    public static class MessagesXml
+    {
+        private static readonly XDocument Doc = XDocument.Load(ProgramDirectory.MessagesPath);
+        
+        public static void SaveMessage(ChatMessage message, string contactId)
+        {
+            lock (Doc)
+            {
+                XElement contactNode = GetContactNode(contactId);
+
+                if (contactNode is null)
+                {
+                    Doc.Root.Add(new XElement("contact", new XAttribute("id", contactId)));
+                    SaveMessage(message, contactId);
+                    return;
+                }
+                
+                contactNode.Add(new XElement(message.Way == ChatMessage.Ways.In ? "IN" : "OUT", message.Text, new XAttribute("id", message.Id)));
+                Doc.Save(ProgramDirectory.MessagesPath);
+            }
+        }
+
+        public static IEnumerable<ChatMessage> GetMessages(int messageCount, string contactId)
+        {
+            XElement contactNode = GetContactNode(contactId);
+            if (contactNode is null)
+                yield break;
+
+            int i = 0;
+            foreach (XElement element in contactNode.Elements())
+            {
+                if (i++ >= messageCount)
+                    yield break;
+                
+                ChatMessage.Ways way = element.Name == "IN" ? ChatMessage.Ways.In : ChatMessage.Ways.Out;
+                string id = element.Attribute("id").Value;
+                string text = element.Value;
+                
+                ChatMessage message = ChatMessage.ReconstructMessage(text, way, id);
+                yield return message;
+            }
+        }
+
+        private static XElement? GetContactNode(string contactId)
+        {
+            lock (Doc)
+            {
+                return Doc.Root?.Elements().FirstOrDefault(node => node.Attribute("id")?.Value == contactId);   
+            }
+        }
+    }
+}
