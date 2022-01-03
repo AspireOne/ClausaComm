a06c3d6fddeb0718488949caaaa1137ee5940853
Matěj Pešl <matejpesl1@gmail.com>
2022-01-03 23:18:29 +0100

    Add time to ChatMessage

--- Messages/ChatMessage.cs
+++ Messages/ChatMessage.cs
@@ -8,6 +8,8 @@ namespace ClausaComm.Messages
     {
         public enum Ways { In, Out }
         public RemoteObject.ObjectType ObjectType => RemoteObject.ObjectType.ChatMessage;
+
+        public readonly long Time;
         public readonly Ways Way;
         public readonly string Text;
         //public readonly MessageFile File;
@@ -19,14 +21,18 @@ namespace ClausaComm.Messages
             if (text?.Length > MaxTextLength)
                 throw new ArgumentException("The text is too long.", nameof(text));
             
-            Text = text;
+            Time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
             Id = IdGenerator.GenerateId(5);
+            Text = text;
             Way = way;
         }
 
-        private ChatMessage(string text, Ways way, string id) : this(text, way) => Id = id;
+        private ChatMessage(string text, Ways way, string id, long time) : this(text, way)
+        {
+            Time = time;
+            Id = id;
+        }
 
-        public static ChatMessage ReconstructMessage(string text, Ways way, string id) =>
-            new ChatMessage(text, way, id);
+        public static ChatMessage ReconstructMessage(string text, Ways way, string id, long time) => new ChatMessage(text, way, id, time);
     }
 }
