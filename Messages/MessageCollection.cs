220452a65754a41145ff115d59ef62f8328003f3
Matěj Pešl <matejpesl1@gmail.com>
2022-01-29 23:04:13 +0100

    Create ChatMessage collection

---
+++ Messages/MessageCollection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ClausaComm.Messages;
+
+/// <summary>
+/// A collection of ChatMessages. No duplicates. Sorted by message send time (index 0 = latest).
+/// </summary>
+public class MessageCollection
+{
+    private readonly List<ChatMessage> Messages = new();
+
+    public void Add(ChatMessage message)
+    {
+        for (int i = 0; i < Messages.Count; ++i)
+        {
+            if (Messages[i] == message)
+                return;
+            
+            if (Messages[i].Time < message.Time)
+            {
+                Messages.Insert(i, message);
+                return;
+            }
+        }
+        
+        Messages.Add(message);
+    }
+    
+    public IEnumerable<ChatMessage> GetAmount(int amount)
+    {
+        for (int i = 0; i < (amount == 0 ? Messages.Count : amount); ++i) 
+            yield return Messages[i];
+    }
+}
