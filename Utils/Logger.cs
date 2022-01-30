fa9e25bfc431a1ac6bcabd792530c2b684ff6e21
Matěj Pešl <matejpesl1@gmail.com>
2022-01-30 12:38:42 +0100

    add a Logger

---
+++ Utils/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace ClausaComm.Utils;
+
+public static class Logger
+{
+    public const int MaxTextLength = 500;
+    public static void Log(string? str)
+    {
+#if !DEBUG
+        return;
+#endif
+        if (str?.Length > MaxTextLength)
+            str = $"{str[..MaxTextLength]}... ({str.Length - MaxTextLength} characters truncated)";
+
+        Debug.WriteLine(str);
+    }
+    
+    public static void Log(object? text) => Log(text?.ToString());
+
+    public static void Log(Func<object> text, bool async = false)
+    {
+#if !DEBUG
+        return;
+#endif
+        
+        if (async)
+            Task.Run(() => Log(text()));
+        else
+            Log(text());
+    }
+}
