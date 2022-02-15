efc454334cc3ba87f44096b31e53caf326d233c8
Matěj Pešl <matejpesl1@gmail.com>
2022-02-15 19:53:28 +0100

    Unsave contact that has the same ip as is currently being assigned

--- Contacts/Contact.cs
+++ Contacts/Contact.cs
@@ -41,8 +41,7 @@ namespace ClausaComm.Contacts
         public static Contact UserContact => _userContact ??=
             XmlFile.Contacts.FirstOrDefault(contact => contact.IsUser)
             ?? new Contact(IpUtils.LocalIp) { Id = IdGenerator.GenerateId(8), IsUser = true, Save = true };
-
-        // TODO: Rework the whole IsUser thing (save just UserId: id and remove he IsUser property)
+        
         public bool IsUser { get; private init; }
         public string ProfilePicPath => Path.Combine(ProgramDirectory.ProfilePicsDirPath, $"{Id}.png");
         private bool HasDefaultProfilePic { get; set; } = true;
@@ -83,7 +82,13 @@ namespace ClausaComm.Contacts
             get => _ip;
             set
             {
-                _ip = value ?? throw new ArgumentNullException(nameof(value), "The supplied IP was null.");
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value), "The supplied IP was null.");
+
+                Contact collidedContact = XmlFile.Contacts?.FirstOrDefault(contact => value.Equals(contact.Ip));
+                if (collidedContact is not null)
+                    collidedContact.Save = false;
+                _ip = value;
             }
         }
 
