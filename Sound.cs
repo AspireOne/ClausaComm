b6d8c0f6acb68fd3df27651c018c260479b7d087
Matěj Pešl <matejpesl1@gmail.com>
2022-02-15 19:43:25 +0100

    Decrease notification sound volume, start sound using Task.Run instead Thread

--- Sound.cs
+++ Sound.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using ClausaComm.Utils;
 using NAudio.Wave;
 
@@ -9,16 +10,16 @@ public static class Sound
 {
     public static void PlayNotificationSound()
     {
-        // TODO: Reuse the thread!
-        ThreadUtils.RunThread(() =>
+        Task.Run(() =>
         {
             using var audioFile =
                 new AudioFileReader(Path.Combine(Directory.GetCurrentDirectory(), @"Resources\notification_sound.mp3"));
+            audioFile.Volume = 0.8f;
             using var outputDevice = new WaveOutEvent();
             outputDevice.Init(audioFile);
             outputDevice.Play();
             while (outputDevice.PlaybackState == PlaybackState.Playing)
-                Thread.Sleep(200); 
+                Thread.Sleep(190);
         });
     }
 }
