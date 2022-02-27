d12fc889c27ac6daa0328f893c9f0418ebe71156
Matěj Pešl <matejpesl1@gmail.com>
2022-02-27 09:52:43 +0100

    Add global utils

---
+++ Utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ClausaComm.Utils;
+
+public static class Utils
+{
+    [DllImport("User32.dll")] private static extern bool SetForegroundWindow(IntPtr handle);
+    [DllImport("User32.dll")] private static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+    [DllImport("User32.dll")] private static extern bool IsIconic(IntPtr handle);
+
+    public static void BringToFront(Process p) {
+        IntPtr handle = p.MainWindowHandle;
+            
+        if (IsIconic(handle))
+            ShowWindow(handle, 9 /* SW_RESTORE*/);
+            
+        SetForegroundWindow(handle);
+    }
+}
