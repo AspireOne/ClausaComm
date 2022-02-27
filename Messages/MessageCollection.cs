616f3c2ff1ed9bc01ac8367fb4fde4c64ec3d1c2
Matěj Pešl <matejpesl1@gmail.com>
2022-02-27 09:53:07 +0100

    refactor, return bool on Add

--- Messages/MessageCollection.cs
+++ Messages/MessageCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -11,30 +12,30 @@ public class MessageCollection
 {
     private readonly List<ChatMessage> Messages = new();
 
-    /// <summary>
-    /// Adds a message to the collection in a sorted manner.
-    /// </summary>
+    /// <summary>Adds a message to the collection in a sorted manner.</summary>
     /// <param name="message">The message to add</param>
-    public void Add(ChatMessage message)
+    /// <returns>True if the message is unique and successfully added; false otherwise.</returns>
+    public bool Add(ChatMessage message)
     {
         for (int i = 0; i < Messages.Count; ++i)
         {
             if (Messages[i] == message)
-                return;
-            
+                return false;
+
             if (Messages[i].Time < message.Time)
             {
                 Messages.Insert(i, message);
-                return;
+                return true;
             }
         }
         
         Messages.Add(message);
+        return true;
     }
     
     /// <param name="amount">The amount of messages to return from the latest (0 for all).</param>
     /// <returns>First {amount} latest messages.</returns>
-    public IEnumerable<ChatMessage> GetAmount(int amount)
+    public IEnumerable<ChatMessage> Get(int amount)
     {
         int cycles = amount == 0 || amount > Messages.Count ? Messages.Count : amount;
         for (int i = 0; i < cycles; ++i) 
